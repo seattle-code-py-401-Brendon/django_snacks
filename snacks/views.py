939145642f6f8bd8bff0b3ec6d737fb6a3cbbb06
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-10 20:35:54 -0700

    finished adding the tests for template and url stuff, finished making home and about page

--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
 
+
 class Homepageview(TemplateView):
     template_name = 'home.html'
+
+
+class Aboutpageview(TemplateView):
+    template_name = 'about.html'
